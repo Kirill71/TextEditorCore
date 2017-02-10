79f70ae8dfcc21b2e67c27754b2b9ee0e1374310
Kirill Zharenkov <extazy1701@gmail.com>
2017-02-10 17:16:56 +0200

    deguf class finder

--- finder.cpp
+++ finder.cpp
@@ -6,17 +6,18 @@ const position & Finder::find_base(const std::string & str, const position & max
 		throw std::logic_error(errorMessage::EMPTY_SEARCH_STRING);
 
 	m_findText.searchString() = str;
-
+	m_findText.lastPosition() = pos;
 	// predicate for algorithm find_if (lambda expression) 
 	auto search_predicate = [](const std::string& current_string,
 		const std::string& search_text, position* find_pos)
 	{
 		++find_pos->m_row;
-		find_pos->m_col = current_string.find(search_text);
+		find_pos->m_col = current_string.find(search_text,(find_pos->m_col != std::string::npos)?find_pos->m_col:0);
 		return  find_pos->m_col != std::string::npos;
 	};
 
-	auto it = std::find_if(container.begin(), container.end(),
+	
+	auto it = std::find_if(container.begin() + pos.m_row , container.end(),
 		std::bind(&search_predicate, std::placeholders::_1,
 			str,&m_findText.lastPosition()));
 
